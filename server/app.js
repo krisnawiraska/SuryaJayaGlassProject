42ff62576b6b5eb96a0b8aae5f4a6f19014160e9
krisnawiraska <krisnaswiraska17@gmail.com>
2023-11-26 17:34:26 +0800

    dashboard

--- server/app.js
+++ server/app.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 const routes = require('./routes')
+const path = require('path')
 
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', (req,res)=>{
+    res.render('index')
+})
 
 app.use('/api', routes)
 app.listen(port, () => {
